839c5fecc87ccc48ffe97ddcd7cc631a52da5df2
Slava2033 <80854817+Slava2033@users.noreply.github.com>
2022-01-16 13:21:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from './contacts.js';
+import { argv } from './lib/yargs.js';
+
+function invokeAction({
+  action,
+  id,
+  name,
+  email,
+  phone,
+}) {
+  switch (action) {
+    case 'list':
+      listContacts();
+      break;
+
+    case 'get':
+      getContactById(id);
+      break;
+
+    case 'add':
+      addContact(name, email, phone);
+      break;
+
+    case 'remove':
+      removeContact(id);
+      break;
+
+    default:
+      console.warn(
+        '\x1B[31m Unknown action type!'
+      );
+  }
+}
+
+invokeAction(argv);
